4c25dc45a625205fd82c545c47e54c406e99b13c
pashanys <pavelcugreev@gmail.com>
2024-11-25 01:48:59 +0300

    start front

--- resources/views/welcome_page/index_pashanys.blade.php
+++ resources/views/welcome_page/index_pashanys.blade.php
@@ -4,8 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Alumni+Sans:ital,wght@0,100..900;1,100..900&family=Cormorant+SC:wght@300&family=Edu+QLD+Beginner:wght@600&family=Kanit&family=Montserrat:wght@100;400&display=swap" rel="stylesheet">
+    <link type="text/css" href="{{url('css/welcome_page/style2.css')}}" rel="stylesheet">
 </head>
 <body>
-    
+    <header class="header">
+        <div class="container">
+            <a href="#" class="header__logo">Talkio.</a>
+            <a href="#" class="header__login"></a>
+        </div>
+    </header>
 </body>
 </html>
