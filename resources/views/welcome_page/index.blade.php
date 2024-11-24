db500c88eba03b19742f66ee26fb62a25459965d
Maksim <m1xg4me@yandex.ru>
2024-11-25 00:16:54 +0300

    remove in own folder

--- resources/views/welcome_page/index.blade.php
+++ resources/views/welcome_page/index.blade.php
@@ -7,7 +7,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css" />
+    <link type="text/css" href="{{url('css/welcome_page/style.css')}}" rel="stylesheet">
 </head>
 <body>
 <header class="header">
