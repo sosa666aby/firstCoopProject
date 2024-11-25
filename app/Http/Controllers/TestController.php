9bcb5945c74c9da5c247817b2593b5082ba3e62a
Maksim <m1xg4me@yandex.ru>
2024-11-25 16:40:52 +0300

    test style2.css + index_pashanys.blade.php

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -8,6 +8,6 @@ class TestController extends Controller
 {
     public function index()
     {
-        return view('welcome_page.index_pashanys');
+        return view('welcome_page.index.');
     }
 }
--- resources/views/welcome_page/index.blade.php
+++ resources/views/welcome_page/index.blade.php
@@ -4,10 +4,13 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-    <link type="text/css" href="{{url('css/welcome_page/style.css')}}" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet"
+    />
+    <link type="text/css" rel="stylesheet" href="{{asset('css/welcome_page/style.css')}}" />
 </head>
 <body>
 <header class="header">
@@ -18,38 +21,45 @@
         </div>
     </div>
 </header>
+<!-- top на main? -->
 <section class="top">
     <div class="container">
-        <div class="top__wrapper">
+        <div class="top__wrapper wrapper">
+            <!-- мб обернуть в li'шки -->
             <div class="top__heading">
-                <h1 class="top__heading-title">Начни свой путь в Talkio.</h1>
+                <h1 class="top__heading-title title">
+                    Начни свой путь в Talkio.
+                </h1>
                 <div class="top__heading-text">
                     <p>
-                        Открой для себя мир общения, обмена идеями и быстрых новостей.
+                        Открой для себя мир общения, обмена идеями и
+                        быстрых новостей.
                     </p>
                     <p>
-                        Поделись своим мнением, следи за трендами и заводи новые
-                        знакомства.
+                        Поделись своим мнением, следи за трендами и
+                        заводи новые знакомства.
                     </p>
                     <p>
-                        Зарегистрируйся, чтобы быть в центре событий и начать свою
-                        уникальную историю прямо сейчас!
+                        Зарегистрируйся, чтобы быть в центре событий и
+                        начать свою уникальную историю прямо сейчас!
                     </p>
                 </div>
                 <div class="top__heading-bottom">
-                    <a class="top__heading-button" href=""> Начать общение </a>
+                    <a class="top__heading-button button" href="">
+                        Начать общение
+                    </a>
                 </div>
             </div>
         </div>
     </div>
     <p class="top__text--rotate">Talkio.</p>
     <div class="bottom">
-
+        <p class="bottom__text">2024 Talkio</p>
     </div>
-    <img class="ringForText" src="images/ringForText.svg" alt="">
-    <img class="ringLeft" src="images/ringLeft.svg" alt="">
-    <img class="topImages" src="images/topImages.svg" alt="">
+    <img class="ringForText" src="images/ringForText.svg" alt="" />
+    <!-- <img class="ringLeft" src="images/ringLeft.svg" alt=""> -->
+    <div class="ringLeft"></div>
+    <img class="topImages" src="images/topImages.svg" alt="" />
 </section>
 </body>
 </html>
-
