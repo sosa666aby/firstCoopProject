95a4d9ff6dbde450a2fed475865991d758a80096
Maksim <m1xg4me@yandex.ru>
2024-11-25 22:24:58 +0300

    auth

---
+++ resources/views/auth/index.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet"
+    />
+    <link rel="stylesheet" href="{{asset("css/welcome_page/style.css")}}" />
+</head>
+<body>
+<header class="header">
+    <div class="container">
+        <div class="header__inner">
+            <a class="logo" href="index.html"> Talkio. </a>
+        </div>
+    </div>
+</header>
+<section class="authorization">
+    <div class="container">
+        <div class="authorization__wrapper wrapper">
+            <div class="authorization__inner">
+                <h1 class="authorization__title title">Вход в аккаунт.</h1>
+                <form class="authorization__form form" action="">
+                    <div class="authorization__input-box input-box">
+                        <input class="authorization__form-input" type="text" placeholder="Адрес электронной почты." />
+                    </div>
+                    <div class="authorization__input-box input-box">
+                        <input class="authorization__form-input" type="password" placeholder="Пароль."/>
+                    </div>
+                </form>
+                <ul class="authorization__items">
+                    <li class="registration-authorization__item">
+                        <button class="authorization__button button">
+                            Войти
+                        </button>
+                    </li>
+                    <li class="registration-authorization__item">
+                        <a class="registration-authorization__item-link" href="registration.html">
+                            Нет аккаунта? Регистрация.
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="bottom">
+        <p class="bottom__text">2024 Talkio</p>
+    </div>
+</section>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
 
 // WELCOME PAGE
 Route::get('/', [IndexController::class, 'index'])->name('index');
@@ -14,8 +15,8 @@ Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
 
 //LOGIN
-//Route::get('/login', [LoginController::class, 'index'])->name('login');
-//
-//Route::post('/login', [LoginController::class, 'store'])->name('login.store');
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+
+Route::post('/login', [LoginController::class, 'store'])->name('login.store');
 
 
