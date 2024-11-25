b217e27b40609597306e91490bd26aed1326023b
Maksim <m1xg4me@yandex.ru>
2024-11-25 11:59:24 +0300

    test style2.css + index_pashanys.blade.php

--- resources/views/welcome_page/index_pashanys.blade.php
+++ resources/views/welcome_page/index_pashanys.blade.php
@@ -7,7 +7,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Alumni+Sans:ital,wght@0,100..900;1,100..900&family=Cormorant+SC:wght@300&family=Edu+QLD+Beginner:wght@600&family=Kanit&family=Montserrat:wght@100;400&display=swap" rel="stylesheet">
-    <link type="text/css" href="{{url('css/welcome_page/style2.css')}}" rel="stylesheet">
+    <link type="text/css" href="{{asset('css/welcome_page/style2.css')}}" rel="stylesheet">
 </head>
 <body>
     <header class="header">
