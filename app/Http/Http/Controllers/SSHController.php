ece3422a936fd68fc1643cbd4daa8077f0bfc16c
Maksim <m1xg4me@yandex.ru>
2024-11-30 20:39:39 +0300

    extend Laravel

---
+++ app/Http/Http/Controllers/SSHController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SSHController extends Controller
+{
+
+    public function sshFromGitLaravel()
+
+    {
+        return "";
+    }
+}
